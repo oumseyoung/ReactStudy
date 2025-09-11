4e2abc6fc5fe9050e4af1532dbf386a738280b99
엄세영 <seyoungoum04@gmail.com>
2025-09-11 15:17:08 +0900

    feat: add useEffect to run code only once after render (#6.1)

--- react-nomad-practice/react-for-beginners/src/App.js
+++ react-nomad-practice/react-for-beginners/src/App.js
@@ -1,11 +1,18 @@
 import Button from "./Button";
 import styles from "./App.module.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
   const onClick = () => setValue((prev) => prev + 1);
-  console.log("render"); // 계속 렌더가 됨. 처음 한 번만 렌더되게 하고 싶은 것도 있을텐데
+  console.log("run all the time"); // 계속 렌더가 됨. 처음 한 번만 렌더되게 하고 싶은 것도 있을텐데 -> 해결책: useEffect
+  const runOnlyOnce = () => {
+    console.log("run only once");
+  };
+  useEffect(() => {
+    console.log("Call the api..."); // 한 번만 실행되는 코드
+  }, []);
+  // useEffect(runOnlyOnce, []);
   return (
     <div>
       <h1 className={styles.title}>Welcome back!</h1>
@@ -17,3 +24,15 @@ function App() {
 }
 
 export default App;
+
+/*
+useEffect는 두 개의 argument를 가지는 function
+첫번째 argument: 딱 한 번만 실행하고 싶은 코드
+두번째 argument: 
+
+useEffect 내에 코드가 실행되는 시점이 컴포넌트의 렌더링 이후
+
+** memo와 useEffect의 차이점은?
+- memo: Props가 변화 되지 않은 등 컴포넌트가 업데이트 되지 않아도 되는 상황 에서 re-redering이 되는 불필요함을 막기 위해 쓰임 (성능 최적화에 가까움)
+- useEffect : 컴포넌트에서 어떠한 변화가 있을 때, 특정 함수에 대한 실행을 제어하는 Hook
+*/
